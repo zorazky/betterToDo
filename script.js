791ec20f01a6d606a99355fe9140b4b6a6510d19
Oswaldo <oswaldo.vazquez@gmail.com>
2019-07-25 23:49:58 -0500

    adding js

---
+++ script.js
@@ -0,0 +1,21 @@
+const listContainer = document.querySelector('[data-lists]')
+
+let lists = ['name', 'todo']
+
+function render() {
+    clearElement(listContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.innerText = list
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
