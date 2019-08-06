052c38979fbd172968259e328f61ecb5db862249
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-06 11:02:27 -0500

    my lists adding and removing working with local storage

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
-let lists = [
-  {
-    id: 1,
-    name: 'name'
-  }, {
-    id: 2,
-    name: 'todo'
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'li') {
+    selectedListId = e.target.dataset.listId
+    console.log(selectedListId)
+    saveAndRender()
   }
-]
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId)
+  selectedListId = null
+  saveAndRender()
+})
 
 newListForm.addEventListener('submit' , e => {
   e.preventDefault()
@@ -19,11 +29,21 @@ newListForm.addEventListener('submit' , e => {
   const list = createList(listName)
   newListInput.value = null
   lists.push(list)
-  render()
+  saveAndRender()
 })
 
 function createList(name) {
-  return { id: Date.now().toString, name: name, tasks: []}
+  return { id: Date.now().toString(), name: name, tasks: []}
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 
@@ -35,6 +55,9 @@ function render() {
     listElement.dataset.listId = list.id
     listElement.classList.add("list-name")
     listElement.innerText = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
     listsContainer.appendChild(listElement)
   })
 }
